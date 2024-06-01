a717d9e1e930824c2d641dd3f4c0cd99cde6a263
Cry0phoenix <Boomshak@yandex.ru>
2024-06-01 23:13:52 +0300

    fourth_push

--- scripts/like.js
+++ scripts/like.js
@@ -10,7 +10,7 @@
 
 const likeHeartArray = document.querySelectorAll('.like-icon');
 const likeButtonArray = document.querySelectorAll('.card__like-button');
-const iconButtonArray = document.querySelectorAll('.card__like-button');
+const iconButtonArray = document.querySelectorAll('.card__icon-button');
 
 iconButtonArray.forEach((iconButton, index) => {
   iconButton.onclick = () =>
